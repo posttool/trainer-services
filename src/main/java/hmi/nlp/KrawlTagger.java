6eed910c8cfe4c513560c1741aae6d99c60697e9
David Karam <david@posttool.com>
2015-05-27 09:52:53 -0700

    fmt

--- src/main/java/hmi/nlp/KrawlTagger.java
+++ src/main/java/hmi/nlp/KrawlTagger.java
@@ -6,13 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
-import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -21,38 +17,23 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
-import edu.stanford.nlp.dcoref.CorefChain;
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.AfterAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.BeforeAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TopicAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.UtteranceAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.trees.LabeledScoredTreeNode;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.trees.TreeCoreAnnotations.TreeAnnotation;
 import edu.stanford.nlp.util.CoreMap;
 
 class KrawlTagger {
     private StanfordCoreNLP pipeline;
     private String basepath;
-    private boolean dollar;
 
     private KrawlTagger(String outfile) throws IOException {
         Properties props = new Properties();
-        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner"); // ,
-                                                                              // parse,
-                                                                              // dcoref
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
         pipeline = new StanfordCoreNLP(props);
         basepath = outfile;
     }
@@ -68,7 +49,6 @@ class KrawlTagger {
             StringBuilder b = new StringBuilder();
             String nerType = null;
             StringBuilder nerB = null;
-            String lastNer = null;
             for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
                 String word = token.get(TextAnnotation.class);
                 // String pos = token.get(PartOfSpeechAnnotation.class);
@@ -76,18 +56,20 @@ class KrawlTagger {
                 // String bf = token.get(BeforeAnnotation.class);
                 String af = token.get(AfterAnnotation.class);
 
-                if (ne.equals(lastNer)) {
+                if (ne.equals(nerType)) {
                     word = processNer(ne, word);
                     nerB.append(word);
                     nerB.append(af);
                 } else {
                     if (nerB != null && !nerType.equals("O")) {
-                        String x = "";
-                        bw1.write("> " + nerType + " " + nerB.toString().trim()+x);
+                        bw1.write("> " + nerType + " " + nerB.toString().trim());
                         bw1.newLine();
                     }
                     nerType = ne;
                     nerB = new StringBuilder();
+                    word = processNer(ne, word);
+                    nerB.append(word);
+                    nerB.append(af);
                 }
 
                 if (word.equals("&")) {
@@ -97,14 +79,14 @@ class KrawlTagger {
                 } else if (word.startsWith("#")) {
                     word = "hashtag " + word.substring(1);
                 } else if (word.equals("``") || word.equals("''") || word.equals("-LRB-") || word.equals("-RRB-")
-                        || word.equals("`")) {
+                        || word.equals("`") || word.equals("″")) {
                     word = "";
                 }
                 b.append(word);
                 b.append(af);
             }
-            if (nerB != null) {
-                bw1.write("> " + nerB + " " + nerType);
+            if (nerB != null && !nerType.equals("O")) {
+                bw1.write("> " + nerType + " " + nerB.toString().trim());
                 bw1.newLine();
             }
             String bb = b.toString().trim();
@@ -113,34 +95,20 @@ class KrawlTagger {
                 bw.newLine();
             }
 
-            // Tree tree = sentence.get(TreeAnnotation.class);
-            // processChild((LabeledScoredTreeNode) tree, 0);
-            //
-            // SemanticGraph dependencies =
-            // sentence.get(CollapsedCCProcessedDependenciesAnnotation.class);
-            // System.out.println(dependencies);
         }
 
-        // This is the coreference link graph
-        // Each chain stores a set of mentions that link to each other,
-        // along with a method for getting the most representative mention
-        // Both sentence and token offsets start at 1!
-        // Map<Integer, CorefChain> graph =
-        // document.get(CorefChainAnnotation.class);
-        // String topics = document.get(TopicAnnotation.class);
-        // System.out.println("!!!" + topics);
-
-        // System.out.println(graph);
     }
 
     private String processNer(String ne, String word) {
-        if (ne.equals("PERSON") || ne.equals("LOCATION") || ne.equals("ORGANIZATION") || ne.equals("MISC")) {
+        if (ne.equals("O")) {
+            return word;
+        } else if (ne.equals("PERSON") || ne.equals("LOCATION") || ne.equals("ORGANIZATION") || ne.equals("MISC")) {
             return word;
         } else if (ne.equals("NUMBER") || ne.equals("DURATION") || ne.equals("ORDINAL") || ne.equals("PERCENT")) {
             return getTextForNumber(word);
         } else if (ne.equals("MONEY")) {
             if (word.equals("$")) {
-                dollar = true;
+                // dollar = true;
                 return "";
             } else {
                 return getTextForNumber(word);
@@ -193,42 +161,6 @@ class KrawlTagger {
         else
             return nword;
     }
-
-    // private void processChild(LabeledScoredTreeNode ltree, int d) {
-    // List<Tree> c = ltree.getChildrenAsList();
-    // StringBuilder b = new StringBuilder();
-    // for (int i = 0; i < d; i++) {
-    // b.append("  ");
-    // }
-    // CoreLabel l = (CoreLabel) ltree.label();
-    // String w = l.word();
-    // if (w != null) {
-    // b.append(w);
-    // } else {
-    // String t = l.value();
-    // b.append(t);
-    // }
-    // for (Tree t : c) {
-    // processChild((LabeledScoredTreeNode) t, d + 1);
-    // }
-    // // System.out.println(b);
-    // }
-
-    // public List<List<CoreLabel>> pos(String text) {
-    // List<List<CoreLabel>> tagged = new ArrayList<List<CoreLabel>>();
-    // // creates a StanfordCoreNLP object, with POS tagging, lemmatization,
-    // // NER, parsing, and coreference resolution
-    // Properties props = new Properties();
-    // props.setProperty("annotators", "tokenize, ssplit, pos");
-    // StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    // Annotation document = new Annotation(text);
-    // pipeline.annotate(document);
-    // List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-    // for (CoreMap sentence : sentences) {
-    // tagged.add(sentence.get(TokensAnnotation.class));
-    // }
-    // return tagged;
-    // }
 
     public void processData() throws IOException {
         Mongo mongoClient = new MongoClient("192.155.87.239");
