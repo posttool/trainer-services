f3a4bd8a01e8247b40bd28b79ae8ead046ce067c
David Karam <david@posttool.com>
2015-07-16 16:15:54 -0700

    import or compute?

--- src/main/java/hmi/synth/voc/train/ASpeechMarkup.java
+++ src/main/java/hmi/synth/voc/train/ASpeechMarkup.java
@@ -2,12 +2,16 @@ package hmi.synth.voc.train;
 
 
 import hmi.annotate.SpeechMarkupAnnotater;
-import hmi.data.SpeechMarkup;
-import hmi.data.VoiceRepo;
+import hmi.data.*;
 import hmi.util.FileList;
 import hmi.util.FileUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ASpeechMarkup {
     VoiceRepo root;
@@ -16,10 +20,12 @@ public class ASpeechMarkup {
     public ASpeechMarkup(String dataDir) throws IOException {
         root = new VoiceRepo(dataDir);
         root.init("/sm");
-        this.ann = new SpeechMarkupAnnotater("en_US");
     }
 
-    public void compute() {
+    public void compute() throws IOException {
+        if (ann == null)
+            ann = new SpeechMarkupAnnotater("en_US");
+
         FileList textFiles = root.textFiles();
         int s = textFiles.length();
         for (int i = 0; i < s; i++) {
@@ -35,9 +41,73 @@ public class ASpeechMarkup {
         }
     }
 
+    public void impose() throws Exception {
+        Map<String, String> mlf2nn = new HashMap<>();
+        FileList mlfs = new FileList(root.path("mlf"), ".txt");
+        for (int c = 0; c < mlfs.length(); c++) {
+            String mlf = FileUtils.getFile(mlfs.file(c));
+            Sentence s = getSetenceFromMlf(mlf.split("\n"));
+            SpeechMarkup sm = new SpeechMarkup();
+            Paragraph pp = new Paragraph();
+            pp.addSentence(s);
+            sm.addParagraph(pp);
+            String name = mlfs.name(c);
+            String newName = "hmi_us_a0_xxx_" + ln(c, 5);
+            mlf2nn.put(name, newName);
+            sm.writeJSON(root.path("sm", newName + ".json"));
+            FileUtils.copy(root.path("wavm", name + ".wav"), root.path("wav", newName + ".wav"));
+            if (c % 100 == 0)
+                System.out.println(s);
+        }
+    }
+
+    private String ln(int c, int l) {
+        String s = "000000000000000" + c;
+        return s.substring(s.length() - l);
+    }
+
+    public Sentence getSetenceFromMlf(String[] lines) {
+        Sentence s = new Sentence();
+        Phrase phrase = new Phrase();
+        s.addPhrase(phrase);
+        Word word = null;
+        Syllable syll = null;
+        for (String line : lines) {
+            String[] ben = line.split("\t");
+            if (ben.length < 3) {
+                System.out.println("Short line " + line);
+                continue;
+            }
+            float b = Float.parseFloat(ben[0]);
+            float e = Float.parseFloat(ben[1]);
+            String n = ben[2].trim();
+            if (b == 0 && e == 0) {
+                if (n.startsWith("_ ")) {
+                    word = new Word();
+                    word.setText(n.substring(2));
+                    syll = new Syllable();
+                    word.addSyllable(syll);
+                    phrase.addWord(word);
+                } else if (n.equals("-")) {
+                    syll = new Syllable();
+                    word.addSyllable(syll);
+                }
+            } else {
+                Phone ph = new Phone();
+                ph.setBegin(b);
+                ph.setEnd(e);
+                ph.setDuration(e - b);
+                ph.setPhone(n);
+                syll.addPhone(ph);
+            }
+        }
+        return s;
+    }
+
     public static void main(String... args) throws Exception {
-        String dataDir = "jbw-vocb";
+        String dataDir = "tom-src";
         ASpeechMarkup asm = new ASpeechMarkup(dataDir);
-        asm.compute();
+        //asm.compute();
+        asm.impose();
     }
 }
