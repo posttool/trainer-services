03cf0d3019092e5f78fbbcb01654883e7142fe83
David Karam <david@posttool.com>
2015-09-30 07:38:10 -0700

    htk patch update

--- deploy/provision-htk.sh
+++ deploy/provision-htk.sh
@@ -3,22 +3,36 @@
 BASE=/Users/posttool/Documents/github/la/deploy
 ROOT=$BASE/install
 PATH=$BASE/bin:$PATH
-
+HTK=HTK-3.4
+HTS=HTS-2.1
+H4H=${HTS}_for_${HTK}
+H4H_URL=http://hts.sp.nitech.ac.jp/archives/2.1/${H4H}.tar.bz2
+AR=ar-for-hts-0.8_for_${HTS}
+AR_URL=http://mi.eng.cam.ac.uk/research/emime/ar-for-hts/archive/${AR}.tar.gz
 ################# HTK
+echo "$HTK $HTS"
 cd $ROOT
-cp /Users/posttool/Documents/github/adapt2/deploy/HTK-3.4.1.tar.gz .
+
+rm -rf htk
+cp /Users/posttool/Documents/github/adapt2/deploy/${HTK}.tar.gz .
+tar -zxf ${HTK}.tar.gz
 cp /Users/posttool/Documents/github/adapt2/deploy/HDecode-3.4.1.tar.gz .
-mkdir -p HTS-patch
-cd HTS-patch
-wget http://hts.sp.nitech.ac.jp/archives/2.2/HTS-2.2_for_HTK-3.4.1.tar.bz2
-# http://hts.sp.nitech.ac.jp/archives/2.3beta/HTS-2.3beta_for_HTK-3.4.1.tar.bz2
-tar -jxvf HTS-2.2_for_HTK-3.4.1.tar.bz2
-cd ..
-tar -zxf HTK-3.4.1.tar.gz
 tar -zxf HDecode-3.4.1.tar.gz
-cd htk
-cp $ROOT/HTS-patch/HTS-2.2_for_HTK-3.4.1.patch .
-patch -p1 -d . < HTS-2.2_for_HTK-3.4.1.patch
+
+mkdir -p htk-patches
+cd htk-patches
+wget ${H4H_URL}
+tar -jxvf ${H4H}.tar.bz2
+wget ${AR_URL}
+tar -zxf ${AR}.tar.gz
+
+cd ../htk
+cp ../htk-patches/${H4H}.patch .
+patch -p1 -d . < ${H4H}.patch
+cp ../htk-patches/${AR}/${AR}.patch .
+patch -p1 -d . < ${AR}.patch
+
+# dk adjust for os x
 sed 's/malloc.h/stdlib.h/' <HTKLib/strarr.c >strarr.c
 cp strarr.c HTKLib/strarr.c
 ./configure -build=i686-apple-macos LDFLAGS=-L/opt/X11/lib CFLAGS='-I/opt/X11/include -I/usr/include/malloc -DARCH=\"darwin\"' --prefix=$ROOT MAXSTRLEN=2048
@@ -26,6 +40,8 @@ make
 make install
 make hdecode
 make install-hdecode
+
+exit
 #
 #
 #
